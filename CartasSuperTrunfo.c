f4605edd9ffa386d7b65b368d6945f20e7ff8b7c
TadeuHenrique93 <tadeupereirasilva34@gmail.com>
2025-08-26 18:56:18 +0000

    cartasSupertrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+    int main (){
+    char estado;
+    char codigo[10];
+    char cidade[20];
+    int populaçao;
+    float área;
+    float PIB;
+    int pontos_turisticos;
+    printf("digite o nome do estado: \n");
+    scanf("%s" , &estado);
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("digite o codigo: \n");
+    scanf("%s",codigo);
 
-    return 0;
-}
+    printf("digite a cidade: \n");
+    scanf("%s",cidade);
+
+    printf("digite a populaçao:  \n");
+    scanf("%d", &populaçao);
+        
+    printf("digite a área:   \n");
+    scanf("%f" , &área);
+
+    printf("digite o PIB:  \n");
+    scanf("%f",&PIB );
+
+    printf("digite o numero de pontos turisticos:  \n");
+    scanf("%d", &pontos_turisticos);
+
+    printf("Populaçao:%d\nárea:%f\n",populaçao,área);
+    printf("PIB:%f\n pontos turisticos:%d\n estado:%s\n",PIB,pontos_turisticos,estado);
+    printf("codigo: %s\n cidade: %s\n",codigo,cidade);
+
+   
+
+
+
+
+
+
+
+ }
+
+
+
